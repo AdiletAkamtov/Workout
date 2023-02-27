e871492ee38edc3f795628ccebf9e8671f3fa5ba
AdiletAkmatov <adiletakmatovofficial@gmail.com>
2023-02-28 02:16:00 +0600

    feat: add prettier settings and styles code

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,14 +1,14 @@
-import { FiArrowLeft } from 'react-icons/fi'
 import { useAuth } from '../../../hooks/useAuth'
 import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
+import { FiArrowLeft } from 'react-icons/fi'
 
 const Header = ({ backLink }) => {
 	const { isAuth } = useAuth()
 	return (
 		<header className={styles.header}>
 			<button onClick={() => {}}>
-				<FiArrowLeft />
+				<FiArrowLeft color='white' />
 			</button>
 			{/* {userprofile} */}
 			<Hamburger />
