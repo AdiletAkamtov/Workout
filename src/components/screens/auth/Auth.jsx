07c34fe1a6483c59fbe5cf1100e45408e36381d0
AdiletAkmatov <adiletakmatovofficial@gmail.com>
2023-03-01 15:03:32 +0600

    fix: fixed Auth pages

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,5 +1,36 @@
+import { useForm } from 'react-hook-form'
+
+import Button from '../../ui/button/Button'
+
+import Layout from '../../layout/Layout'
+
 const Auth = () => {
-	return <div>Auth</div>
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		console.log(data)
+	}
+	return (
+		<Layout heading='Sign in' bgImage='/images/auth-bg.png'>
+			<div className='wrapper-inner-page'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='email'
+						placeholder='Enter email'
+						{...register('email', { required: 'Email is required' })}
+					/>
+					<div style={{ color: 'red' }}></div>
+					<Button>Lets go</Button>
+				</form>
+			</div>
+		</Layout>
+	)
 }
 
 export default Auth
